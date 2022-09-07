1f68e12735efcd0b6d28a3a98881eca07cec1842
gustavocgb11@gmail.com <gustavocgb11@gmail.com>
2022-09-07 20:22:27 -0300

    feat: component FormTop

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <main class="colunms is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline">
     <div class="column is-one-quarter">
       <BarraLateral />
     </div>
     <div class="column is-three-quarter">
-
+      <FormTop />
     </div>
   </main>
 </template>
@@ -12,12 +12,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
+import FormTop from './components/Formulario.vue';
 
 export default defineComponent({
     name: "App",
-    components: { 
-      BarraLateral
-    }
+    components: {
+    BarraLateral,
+    FormTop
+}
 });
 </script>
 
