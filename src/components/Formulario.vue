131840c8c952887ce75c6352365ed2139fc3f714
gustavocgb11@gmail.com <gustavocgb11@gmail.com>
2022-09-07 20:31:42 -0300

    refactor: component FormTop

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -4,23 +4,23 @@
       <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
         <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
       </div>
-    </div>
-    <div class="columns">
-      <section>
-        <strong>00:00:00</strong>
-      </section>
-      <button class="button">
-        <span class="icon">
-          <i class="fas fa-play"></i>
-        </span>
-        <span>Play</span>
-      </button>
-      <button class="button">
-        <span class="icon">
-          <i class="fas fa-stop"></i>
-        </span>
-        <span>Stop</span>
-      </button>
+      <div class="column">
+        <section>
+          <strong>00:00:00</strong>
+        </section>
+        <button class="button">
+          <span class="icon">
+            <i class="fas fa-play"></i>
+          </span>
+          <span>Play</span>
+        </button>
+        <button class="button">
+          <span class="icon">
+            <i class="fas fa-stop"></i>
+          </span>
+          <span>Stop</span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
